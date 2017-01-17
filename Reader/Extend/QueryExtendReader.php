a02fbfc8e9a587d05811b35409cb46aa81ed9561
mapserver2007 <mapserver2007@gmail.com>
2017-01-18 00:00:46 +0900

    クエリ情報取得処理を実装

--- Reader/Extend/QueryExtendReader.php
+++ Reader/Extend/QueryExtendReader.php
@@ -29,6 +29,35 @@ class QueryExtendReader extends ExtendReader
      */
     public function read(array $annotationInfoList)
     {
-        var_dump($annotationInfoList);
+        $func = function ($queryKey, $xpath) use ($annotationInfoList) {
+            var_dump("hoge");
+            $query = null;
+            foreach ($annotationInfoList as $annotationInfo) {
+                $queryFunctions = $annotationInfo[$queryKey];
+                if ($queryFunctions === null) {
+                    continue;
+                }
+                foreach ($queryFunctions as $queryFunction) {
+                    $xmlObjectList = $queryFunction->fetch();
+                    foreach ($xmlObjectList as $xmlObject) {
+                        if ($xmlObject !== null) {
+                            $xmlElement = $xmlObject->xpath($xpath);
+                            if (!empty($xmlElement)) {
+                                $query = ["sql" => trim($xmlElement[0]->__toString()), "method" => $xmlElement[0]->getName()];
+                                $entity = $xmlElement[0]->attributes()["entity"];
+                                $query["entity"] = $entity !== null ? $entity->__toString() : null;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+
+            return $query;
+        };
+
+        $this->annotationInfo = function ($queryKey, $xpath) use ($func) {
+            return $func($queryKey, $xpath);
+        };
     }
 }
