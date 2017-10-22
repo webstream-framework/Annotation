d017d58fd96017c33de09a5fe10223c8bdfb83ba
mapserver2007 <mapserver2007@gmail.com>
2017-10-23 00:17:55 +0900

    一旦依存モジュールを削除

--- Modules/ClassLoader/Test/Fixtures/ImportFixture6.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace WebStream\ClassLoader\Test\Fixtures;
-
-class ImportFixture6
-{
-}
--- Modules/ClassLoader/Test/Fixtures/NamespaceSub/Sub1/ImportFixture7.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace WebStream\ClassLoader\Test\Fixtures\NamespaceSub\Sub1;
-
-class ImportFixture7
-{
-}
